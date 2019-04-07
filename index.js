c5149ab492a86724d0549acedab548e4edde7da3
Kamil Dzieniszewski <kamil.dzieniszewski@daftcode.pl>
2019-04-07 11:11:51 +0200

    add router 🔥

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var path = require('path');
 const express = require('express')
+var path = require('path');
 
 require('dotenv').config({
     path: path.join(__dirname, 'config', 'app.env')
 })
 
 const app = express()
+
+require('./web/routing/base.router')(app)
 
 app.listen(process.env.PORT, () => {
     console.log(
